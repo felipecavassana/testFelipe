ac8b718443edfdfa504bc58aad30cd1cf04b2fd5
Felipe Cavassana de Carvalho <felipe.cavassana@hotmail.com>
2014-02-18 17:09:08 -0300

    new

--- src/MeuPrograma.java
+++ src/MeuPrograma.java
@@ -2,25 +2,24 @@
 public class MeuPrograma {	
 	public static void main(String[] args){
 		
-		String user_profile = "00eF00000014579";
+		String ref1 = "<ul><li>Order : 123456789012 </li></ul>";
+		String ref2 = "<ul><li>Order : 12121xxx2121 </li></ul>";
+		String cut = "Order : ";  
 		
-		Cliente c1 = new Cliente();
-		c1.setNome("Felipe");
+		//Cliente c1 = new Cliente();
+		//c1.setNome("Felipe");
 				
-		System.out.println(Conta.getTotalDeContas());
+		//System.out.println(Conta.getTotalDeContas());
 
+		ref1 = ref1.substring(ref1.indexOf(cut)+cut.length()); 
+        ref1 = ref1.substring(0,ref1.indexOf('<')).trim();
+        
+        ref2 = ref2.substring(ref2.indexOf(cut)+cut.length()); 
+        ref2 = ref2.substring(0,ref2.indexOf('<')).trim();
 		
-		if(user_profile != "00eF00000014579" || user_profile != "00eF0000000SZDj" || user_profile != "00eF0000000Req5" || user_profile != "00eF0000000ReWz"){
-			System.out.println("1 - Entrou!");
-		}else{
-			System.out.println("1 - Não entrou!");
-		}
+		System.out.println(Long.valueOf(ref1));
+		System.out.println(Long.valueOf(ref2));
 		
-		if(user_profile != "00eF00000014579" && user_profile != "00eF0000000SZDj" && user_profile != "00eF0000000Req5" && user_profile != "00eF0000000ReWz"){
-			System.out.println("2 - Entrou!");
-		}else{
-			System.out.println("2 - Não entrou!");
-		}
 		
 	}
 }
